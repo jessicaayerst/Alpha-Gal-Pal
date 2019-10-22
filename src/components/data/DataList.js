d831d3040cccf8c57f94f8ae02ca3c148cab01f4
newforce38 <jessicaayerst@gmail.com>
2019-10-22 11:47:08 -0400

    one working section in data

--- src/components/data/DataList.js
+++ src/components/data/DataList.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
-
+import allergenManager from '../../modules/allergenManager'
 
 class DataList extends Component {
+    state = {
+        allergens: [],
+    }
+
+    componentDidMount(){
+        allergenManager.getAll()
+        .then((allergensFromData) => {
+            console.log(allergensFromData)
+            this.setState({
+                allergens: allergensFromData
+            })
+            this.getNumberTrueTookMeds()
+            console.log(allergensFromData.length)
+        })
+    }
+
+getNumberTrueTookMeds(){
+    allergenManager.getAll()
+    .then((allergensFromData) => {
+        console.log(allergensFromData)
+        var result = 0;
+        for(let i = 0; allergensFromData.length > i; i++){
+            if(allergensFromData[i].tookMeds === true){
+                result++;
+            }
+        }
+        var percentTookMeds = (result/allergensFromData.length)*100
+        console.log(percentTookMeds);
+        this.setState({
+            tookMeds: percentTookMeds
+        })
+    })
+
+}
 
     render() {
         return <div className="card">
             <div className="card-content">
-            <p>Data</p>
+            <h3>Data Analysis</h3>
+            <p>Users had to take medication for <h3>{this.state.tookMeds} %</h3> of reactions.</p>
             </div>
         </div>
 
